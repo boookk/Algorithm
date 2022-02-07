3229f4136b4f86c66acf27d905d1b406307938d5
BoGyeong Kang <76933244+boookk@users.noreply.github.com>
2022-02-07 22:58:32 +0900

    Update 1018_체스판 다시칠하기.py
    Modify annotations for variables w and b.
    Delete another comment.

--- 브루트 포스/1018_체스판 다시칠하기.py
+++ 브루트 포스/1018_체스판 다시칠하기.py
@@ -6,16 +6,16 @@ for _ in range(n):
 result = 100
 for i in range(n-7):
     for j in range(m-7):
-        w = 0
-        b = 0
+        w = 0                           # 체스판이 흰색으로 시작하는 경우
+        b = 0                           # 검정색으로 시작하는 경우
         for y in range(i, i+8):
             for x in range(j, i+8):
                 if (x + y) % 2 == 0:
-                    if board[y][x] == 'W': b += 1 # 짝수 칸이 검정이어야 하는데, 흰색인 경우
-                    else: w += 1 # 짝수 칸이 흰색이어야 하는데 검정색인 경우
+                    if board[y][x] == 'W': b += 1
+                    else: w += 1
                 else:
-                    if board[y][x] == 'B': b += 1 # 홀수 칸이 흰색이어야 하는데 검정인 경우
-                    else: w += 1 # 홀수 칸이 검정이어야 하는데 검정인경우
+                    if board[y][x] == 'B': b += 1
+                    else: w += 1
         result = min(w, b, result)
 
 print(result)
