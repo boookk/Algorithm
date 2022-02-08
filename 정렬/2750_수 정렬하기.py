64a0ed1211d0fbc51a34906d8bac6ee4b7bd19ce
BoGyeong Kang <76933244+boookk@users.noreply.github.com>
2022-02-08 23:19:45 +0900

    Update 2750_수 정렬하기.py
    Add Insertion sort code.

--- 정렬/2750_수 정렬하기.py
+++ 정렬/2750_수 정렬하기.py
@@ -10,13 +10,12 @@ for num in number:
     print(num)
 
 
-""" 현재 풀이 """
+""" 버블 정렬 """
 n = int(input())
 numbers = []
 for _ in range(n):
     numbers.append(int(input()))
 
-""" 버블 정렬 """
 for i in range(len(numbers)):
     for j in range(0, len(numbers)-i-1):
         if numbers[j] > numbers[j+1]:
@@ -24,7 +23,27 @@ for i in range(len(numbers)):
 
 print(*numbers, sep='\n')
 
+
+
+""" 삽입 정렬 """
+n = int(input())
+numbers = []
+for _ in range(n):
+    numbers.append(int(input()))
+
+for i in range(1, n):
+    tmp = numbers[i]
+    idx = i - 1
+    while tmp < numbers[idx] and idx > -1:
+        numbers[idx+1] = numbers[idx]
+        idx -= 1
+    numbers[idx+1] = tmp
+
+print(*numbers, sep='\n')
+
+
+
 """
 예전에 풀었을 때는 라이브러리를 사용하여 빠르게 풀었다.
-현재는 라이브러리를 사용하지 않고 버블 정렬 알고리즘을 구현하여 문제를 해결하였다.
+현재는 라이브러리를 사용하지 않고 버블 정렬 알고리즘과 삽입 정렬 알고리즘을 구현하여 문제를 해결하였다.
 """
