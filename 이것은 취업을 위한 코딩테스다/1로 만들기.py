b3a55bbb612f80f3a57c8de2bf78901e068c177e
BoGyeong Kang <76933244+boookk@users.noreply.github.com>
2022-02-25 14:53:52 +0900

    Create 1로 만들기.py

---
+++ 이것은 취업을 위한 코딩테스다/1로 만들기.py
@@ -0,0 +1,15 @@
+""" 다이나믹 프로그맹 (보텀업 방식) """
+
+n = int(input())
+dp = [0] * 30001
+
+for i in range(2, n+1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+print(dp[n])
