b15a53c7e6658df17b8933561417f79ba573c206
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-12-02 13:38:25 +0900

    Create 1978_소수 찾기.py

---
+++ Math/1978_소수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+answer = 0
+
+for number in numbers:
+    if number < 2:
+        continue
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            break
+    else:
+        answer += 1
+
+print(answer)
