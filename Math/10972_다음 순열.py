f1d6637bfa3aae87ea617cf6dda03bb7844808f6
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-12-07 13:02:48 +0900

    Create 10972_다음 순열.py

---
+++ Math/10972_다음 순열.py
@@ -0,0 +1,13 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+for i in range(n - 1, 0, -1):
+    if numbers[i - 1] < numbers[i]:
+        for j in range(n - 1, 0, -1):
+            if numbers[i - 1] < numbers[j]:
+                numbers[i - 1], numbers[j] = numbers[j], numbers[i - 1]
+                numbers = numbers[:i] + sorted(numbers[i:])
+                print(*numbers)
+                exit(0)
+
+print(-1)
